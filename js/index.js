7e25a1f1d81ff6e4a443a2e30988a1276607cabf
Mai Anh Bắc <maianhbac17052001@gmail.com>
2023-12-03 12:30:54 +0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,33 @@
+// const home = document.getElementById("home")
+// const about = document.getElementById("about")
+// const project = document.getElementById("project")
+// const contact = document.getElementById("contact")
+// const actived = document.getElementById("hh")
+// about.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     about.classList.add("active")
+//     home.classList.remove("active")
+//   });
+// home.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     about.classList.remove("active")
+//     project.classList.remove("active")
+//     contact.classList.remove("active")
+//     home.classList.add("active")
+//     actived.classList.add("actived")
+//   });
+// project.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     project.classList.add("active")
+//     home.classList.remove("active")
+//     about.classList.remove("active")
+//     contact.classList.remove("active")
+//   });
+// contact.addEventListener('click', function(event) {
+//     event.preventDefault();
+//     contact.classList.add("active")
+//     home.classList.remove("active")
+//     project.classList.remove("active")
+//     about.classList.remove("active")
+//     actived.classList.remove("actived")
+//   });
